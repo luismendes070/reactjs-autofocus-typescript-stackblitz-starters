534860c6330cfc80fee7d4a4b911792660462b70
Luis Mendes <machadoluismendes@gmail.com>
2023-07-15 14:34:32 -0300

    Increment
    babel
    webpack
    path-browserify

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,39 @@ import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
+import _ from 'lodash';
+// import printMe from './print.js';
+
+function component() {
+  const element = document.createElement('div');
+  const btn = document.createElement('button');
+
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+  return element;
+}
+
+document.body.appendChild(component());
+
+if (module.hot) {
+ module.hot.accept('./print.js', function() {
+   console.log('Accepting the updated printMe module!');
+   printMe();
+ })
+}
+
 require("@babel/core").transform("code", {
   presets: ["@babel/preset-env"],
 });
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
+}
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
