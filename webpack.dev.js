fcd9dfd667ea46a7f9af345340b62b9633d4a5d0
Luis Mendes <machadoluismendes@gmail.com>
2023-07-16 06:44:28 -0300

    Increment
    webpack output

---
+++ webpack.dev.js
@@ -0,0 +1,11 @@
+// https://webpack.js.org/guides/production/
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+});
