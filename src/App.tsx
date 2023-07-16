5258ed55600170d03d020ba5b4541f1e2189c3b4
Luis Mendes <machadoluismendes@gmail.com>
2023-07-16 06:38:43 -0300

    Increment
    webpack output

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import * as React from 'react';
 import './style.css';
 
-import React from "react";
 import MyComponent from "./MyComponent"
-// import InputWithAutoFocus from "./InputWithAutoFocus";
+import InputWithAutoFocus from "./InputWithAutoFocus";
 
 const App = () => {
   return (
     <div>
-      <MyComponent />
-      
+      <MyComponent/>
+      <InputWithAutoFocus/>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
     
---
+++ src/print.js
@@ -0,0 +1,4 @@
+// https://webpack.js.org/guides/output-management/
+export default function printMe() {
+    console.log('I get called from print.js!');
+  }
