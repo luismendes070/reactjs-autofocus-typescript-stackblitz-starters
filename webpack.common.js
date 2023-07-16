a6ade27c061f3afe06fb93518be853c1f2fa4373
Luis Mendes <machadoluismendes@gmail.com>
2023-07-16 06:57:38 -0300

    Increment
    babel-core
    babel-jest
    @babel/preset-env
    babel-loader webpack

--- webpack.common.js
+++ webpack.common.js
@@ -16,4 +16,18 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 };
